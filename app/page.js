8aa44feea322fd712f81bd39f5e4e2296f16c51b
Md Azad Hossain <sokalazad@gmail.com>
2024-05-11 01:23:54 +0200

    added suspance to the location detector

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import { Suspense } from "react";
 import LocationDetector from "./components/LocationDetector";
 
 
 export default function Home() {
   return (
+    <Suspense fallback="fetching location...">
     <LocationDetector />
+    </Suspense>
   );
 }
