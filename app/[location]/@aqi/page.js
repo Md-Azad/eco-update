646bc3a9ac57b4bee2a0fe058318f0a43eeea1e4
Md Azad Hossain <sokalazad@gmail.com>
2024-05-11 00:48:24 +0200

    created some parallel routes

---
+++ app/[location]/@aqi/page.js
@@ -0,0 +1,9 @@
+
+export default function Aqipage (){
+    return(
+        <div>
+            <h1>alsdkj  Page</h1>
+            
+        </div>
+    )
+}
---
+++ app/[location]/page.js
@@ -0,0 +1,6 @@
+
+export default function Aqipage() {
+    return (
+      <div>Home page aqi </div>
+    );
+  }
